310241277c2d27aaedc5dd86d8c332ff0700ae53
paallaire <pascalandre.allaire@gmail.com>
2023-08-04 20:02:30 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Expertises } from "./components/Expertises";
+
+function App() {
+  return (
+    <>
+      <div className=" overflow-hidden">
+        <div className="h-screen bg-indigo-600"></div>
+        <Expertises />
+        <div className="h-screen bg-indigo-600"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
